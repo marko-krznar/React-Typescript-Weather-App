8031e7085ba45a76b6de4f6c193c8347edd69c87
marko <krznarm26@gmail.com>
2022-07-10 00:27:58 +0200

    Added 5 day weather forecast

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,20 @@ interface IWeather {
   };
 }
 
+interface IFiveDayWeather {
+  fiveDayWeather: [
+    {
+      dt_txt: string;
+      weather: [
+        {
+          main: string;
+          description: string;
+        }
+      ];
+    }
+  ];
+}
+
 function App() {
   const url = "https://api.openweathermap.org/";
   const apiKey = "bfd14f25f12b27477818a553e86af0d4";
@@ -50,6 +64,22 @@ function App() {
     },
   });
 
+  const [fiveDayWeatherInfo, setFiveDayWeatherInfo] = useState<IFiveDayWeather>(
+    {
+      fiveDayWeather: [
+        {
+          dt_txt: "",
+          weather: [
+            {
+              main: "",
+              description: "",
+            },
+          ],
+        },
+      ],
+    }
+  );
+
   useEffect(() => {
     axios
       .get(`${url}geo/1.0/direct?q=Zagreb&limit=5&appid=${apiKey}`)
@@ -63,22 +93,52 @@ function App() {
       .then((res) => {
         setWeatherInfo({ weather: res.data });
       });
+    axios
+      .get(`${url}data/2.5/forecast?q=Zagreb&units=metric&appid=${apiKey}`)
+      .then((res) => {
+        setFiveDayWeatherInfo({ fiveDayWeather: res.data.list });
+      });
   }, []);
+
+  // console.log(fiveDayWeatherInfo);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather app</h1>
+        <h1 className="font-bold leading-tight text-5xl mt-12 mb-12 text-center">
+          Weather app
+        </h1>
       </header>
-      <p>Town: {location.locations[0].name}</p>
-      <p>Country: {location.locations[0].country}</p>
-      <div className="card">
+
+      <div className="max-w-2xl px-8 py-4 mx-auto mb-12 bg-white rounded-lg shadow-md">
+        Current Weather Data
+        <h2 className="text-2xl font-bold">
+          {location.locations[0].name}, {location.locations[0].country}
+        </h2>
         <p>Temp: {weatherInfo.weather.main.temp}</p>
         <p>Feels like: {weatherInfo.weather.main.feels_like}</p>
         <p>Min temp: {weatherInfo.weather.main.temp_min}</p>
         <p>Max temp: {weatherInfo.weather.main.temp_max}</p>
         <p>Pressure: {weatherInfo.weather.main.pressure}</p>
         <p>Humidity: {weatherInfo.weather.main.humidity}</p>
+      </div>
+
+      <div className="p-6">
+        <h2 className="text-2xl font-bold mb-6">5 day weather forecast</h2>
+        <ul className="flex flex-wrap">
+          {fiveDayWeatherInfo.fiveDayWeather.map((item, i) => {
+            return (
+              <li
+                className="max-w-xs px-8 py-4 mx-auto bg-white rounded-lg shadow-md my-2 flex-auto"
+                key={i}
+              >
+                <span className="block">{item.dt_txt}</span>
+                <span className="block">{item.weather[0].main}</span>
+                <span className="block">{item.weather[0].description}</span>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
