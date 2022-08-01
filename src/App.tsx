c307291bfcf30d78601c407ee98f2fef5e9d33ed
marko <krznarm26@gmail.com>
2022-08-01 20:27:31 +0200

    Changed ui & added more weather information

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { BsCloudSun } from "react-icons/bs";
+import { BsSunset, BsSunrise, BsWind } from "react-icons/bs";
 import { FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
-import { WiHumidity } from "react-icons/wi";
+import { IoLocationOutline } from "react-icons/io5";
 
 interface ILocation {
   locations: [
@@ -16,6 +16,13 @@ interface ILocation {
 
 interface IWeather {
   weather: {
+    weather: [
+      {
+        main: string;
+        description: string;
+        icon: string;
+      }
+    ];
     main: {
       temp: number;
       feels_like: number;
@@ -23,6 +30,14 @@ interface IWeather {
       temp_max: number;
       pressure: number;
       humidity: number;
+    };
+    sys: {
+      sunrise: number;
+      sunset: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
     };
   };
 }
@@ -56,6 +71,13 @@ function App() {
 
   const [weatherInfo, setWeatherInfo] = useState<IWeather>({
     weather: {
+      weather: [
+        {
+          main: "",
+          description: "",
+          icon: "",
+        },
+      ],
       main: {
         temp: 0,
         feels_like: 0,
@@ -63,6 +85,14 @@ function App() {
         temp_max: 0,
         pressure: 0,
         humidity: 0,
+      },
+      sys: {
+        sunrise: 0,
+        sunset: 0,
+      },
+      wind: {
+        speed: 0,
+        deg: 0,
       },
     },
   });
@@ -103,38 +133,70 @@ function App() {
       });
   }, []);
 
-  // console.log(fiveDayWeatherInfo);
-
   return (
-    <div className="App">
+    <div className="App px-4">
       <header className="App-header">
-        <h1 className="font-bold leading-tight text-5xl mt-12 mb-12 text-center">
+        <h1 className="font-bold leading-tight text-5xl mt-12 mb-12 text-center text-white">
           Current Weather
         </h1>
       </header>
 
-      <div className="max-w-2xl px-8 py-4 mx-auto mb-12 bg-white/[.2] rounded-lg shadow-md">
-        <div className="flex justify-center items-center">
-          <BsCloudSun className="text-9xl px-4" />
+      <div className="max-w-2xl px-8 py-4 mx-auto mb-12 bg-white/[.2] rounded-lg shadow-md backdrop-blur-lg text-white">
+        <div className="flex justify-center items-center pt-6">
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherInfo?.weather?.weather[0]?.icon}@4x.png`}
+            alt={`${weatherInfo.weather.weather[0].description}`}
+          />
           <div className="px-4">
-            <p className="text-4xl">{weatherInfo.weather.main.temp}° C</p>
-            <h2 className="text-base">
+            <p className="text-5xl font-bold">
+              {weatherInfo.weather.main.temp}° C
+            </p>
+            <h2 className="text-base flex items-center mt-2">
+              <IoLocationOutline className="mr-2" />
               {location.locations[0].name}, {location.locations[0].country}
             </h2>
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center pb-4">
+          <div className="flex align-center flex-col px-4">
+            <div className="text-center text-2xl">
+              <BsSunrise className="inline" />
+            </div>
+            <p className="font-bold">
+              {new Date(
+                weatherInfo.weather.sys.sunrise * 1000
+              ).toLocaleTimeString()}
+            </p>
+          </div>
           <div className="flex align-center flex-col px-4">
             <div className="text-center text-2xl">
               <FaTemperatureHigh className="inline" />
             </div>
-            <p>{weatherInfo.weather.main.temp_min}° C</p>
+            <p className="font-bold">{weatherInfo.weather.main.temp_min}° C</p>
+          </div>
+          <div className="flex align-center flex-col px-4">
+            <div className="text-center text-2xl">
+              <BsWind className="inline" />
+            </div>
+            <p className="font-bold">
+              {(weatherInfo.weather.wind.speed * 3.6).toFixed(2)} km/h
+            </p>
           </div>
           <div className="flex align-center flex-col px-4">
             <div className="text-center text-2xl">
               <FaTemperatureLow className="inline" />
             </div>
-            <p>{weatherInfo.weather.main.temp_max}° C</p>
+            <p className="font-bold">{weatherInfo.weather.main.temp_max}° C</p>
+          </div>
+          <div className="flex align-center flex-col px-4">
+            <div className="text-center text-2xl">
+              <BsSunset className="inline" />
+            </div>
+            <p className="font-bold">
+              {new Date(
+                weatherInfo.weather.sys.sunset * 1000
+              ).toLocaleTimeString()}
+            </p>
           </div>
         </div>
       </div>
