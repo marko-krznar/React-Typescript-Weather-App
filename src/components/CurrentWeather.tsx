faf81dd001613114462abffeb718888062eaf192
marko <krznarm26@gmail.com>
2022-08-21 18:09:47 +0200

    Adeed custom hook for API manipulation & adjusted code accordingly

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {
     BsSunset,
@@ -12,88 +11,73 @@ import { FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
 import { IoLocationOutline } from "react-icons/io5";
 import { fromUnixTime, format } from "date-fns";
 
+import { apiKey, useAxios } from "../api/hookAxios";
+
 interface ICurrentWeather {
-    weather: {
-        weather: [
-            {
-                id: number;
-                main: string;
-                description: string;
-                icon: string;
-            }
-        ];
-        main: {
-            temp: number;
-            feels_like: number;
-            temp_min: number;
-            temp_max: number;
-            pressure: number;
-            humidity: number;
-        };
-        sys: {
-            sunrise: number;
-            sunset: number;
-        };
-        wind: {
-            speed: number;
-            deg: number;
-        };
+    weather: [
+        {
+            id: number;
+            main: string;
+            description: string;
+            icon: string;
+        }
+    ];
+    main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        humidity: number;
     };
-    errorMsg: string;
+    sys: {
+        sunrise: number;
+        sunset: number;
+    };
+    wind: {
+        speed: number;
+        deg: number;
+    };
 }
 
 const CurrentWeather = () => {
-    const url = "https://api.openweathermap.org/";
-    const apiKey = "bfd14f25f12b27477818a553e86af0d4";
+    const { response, loading, error }: any = useAxios({
+        method: "GET",
+        url: `data/2.5/weather?lat=45.8131847&lon=15.9771774&units=metric&appid=${apiKey}`,
+    });
 
-    const [weatherInfo, setWeatherInfo] = useState<ICurrentWeather>({
-        weather: {
-            weather: [
-                {
-                    id: 0,
-                    main: "",
-                    description: "",
-                    icon: "",
-                },
-            ],
-            main: {
-                temp: 0,
-                feels_like: 0,
-                temp_min: 0,
-                temp_max: 0,
-                pressure: 0,
-                humidity: 0,
+    const [data, setData] = useState<ICurrentWeather>({
+        weather: [
+            {
+                id: 0,
+                main: "",
+                description: "",
+                icon: "",
             },
-            sys: {
-                sunrise: 0,
-                sunset: 0,
-            },
-            wind: {
-                speed: 0,
-                deg: 0,
-            },
+        ],
+        main: {
+            temp: 0,
+            feels_like: 0,
+            temp_min: 0,
+            temp_max: 0,
+            pressure: 0,
+            humidity: 0,
         },
-        errorMsg: "",
+        sys: {
+            sunrise: 0,
+            sunset: 0,
+        },
+        wind: {
+            speed: 0,
+            deg: 0,
+        },
     });
 
     useEffect(() => {
-        axios
-            .get(
-                `${url}data/2.5/weather?lat=45.8131847&lon=15.9771774&units=metric&appid=${apiKey}`
-            )
-            .then((res) => {
-                setWeatherInfo({
-                    ...weatherInfo,
-                    weather: res.data,
-                });
-            })
-            .catch((err) => {
-                setWeatherInfo({
-                    ...weatherInfo,
-                    errorMsg: err.message,
-                });
-            });
-    }, []);
+        if (response !== null) {
+            setData(response);
+        }
+    }, [response]);
 
     const renderIcon = (
         iconCode: string,
@@ -111,103 +95,108 @@ const CurrentWeather = () => {
         return (
             <img
                 src={`https://openweathermap.org/img/wn/${iconCode}@${iconSize}x.png`}
-                alt={`${weatherInfo.weather.weather[0].description}`}
+                alt={`${data.weather[0].description}`}
             />
         );
     };
 
     return (
         <section className="px-8 py-4 mx-auto mb-12 mt-12 bg-white/[.2] rounded-lg shadow-md backdrop-blur-lg text-white">
-            {weatherInfo.errorMsg ? (
-                <div className="text-center my-4">
-                    <BsEmojiFrown className="text-6xl mb-6 mx-auto" />
-                    <p className="text-3xl mb-2">
-                        Aaaaaaaah! Something went wrong
-                    </p>
-                    <p>{weatherInfo.errorMsg}</p>
-                </div>
+            {loading ? (
+                <div>loading...</div>
             ) : (
                 <>
-                    <div className="flex justify-center items-center pt-6 flex-col border-b-2 border-white mb-6 sm:flex-row sm:border-none">
-                        <div className="m-8">
-                            {renderIcon(
-                                weatherInfo?.weather?.weather[0]?.icon,
-                                4,
-                                "text-8xl"
-                            )}
-                        </div>
-                        <div className="px-4 mb-8 sm:mb-0">
-                            <p className="text-5xl font-bold text-center">
-                                {Math.round(weatherInfo.weather.main.temp)}° C
+                    {error && (
+                        <div className="text-center my-4">
+                            <BsEmojiFrown className="text-6xl mb-6 mx-auto" />
+                            <p className="text-3xl mb-2">
+                                Aaaaaaaah! Something went wrong
                             </p>
-                            <h2 className="text-base flex justify-center items-center mt-2 sm:justify-start">
-                                <IoLocationOutline className="mr-2 text-2xl" />
-                                <span className="opacity-80">
-                                    Zagreb, Hrvatska
-                                </span>
-                            </h2>
+                            <p>{error.message}</p>
                         </div>
-                    </div>
-                    <div className="flex justify-center pb-4 flex-wrap">
-                        <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
-                            <div className="text-center text-2xl">
-                                <BsSunrise className="inline text-5xl" />
-                            </div>
-                            <p className="text-center mt-2 opacity-80">
-                                {format(
-                                    new Date(
-                                        fromUnixTime(
-                                            weatherInfo.weather.sys.sunrise
-                                        )
-                                    ),
-                                    "HH:mm"
-                                )}
-                            </p>
-                        </div>
-                        <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
-                            <div className="text-center text-2xl">
-                                <FaTemperatureHigh className="inline text-5xl" />
-                            </div>
-                            <p className="text-center mt-2 opacity-80">
-                                {Math.round(weatherInfo.weather.main.temp_min)}°
-                                C
-                            </p>
-                        </div>
-                        <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
-                            <div className="text-center text-2xl">
-                                <BsWind className="inline text-5xl" />
-                            </div>
-                            <p className="text-center mt-2 opacity-80">
-                                {(weatherInfo.weather.wind.speed * 3.6).toFixed(
-                                    2
-                                )}{" "}
-                                km/h
-                            </p>
-                        </div>
-                        <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
-                            <div className="text-center text-2xl">
-                                <FaTemperatureLow className="inline text-5xl" />
-                            </div>
-                            <p className="text-center mt-2 opacity-80">
-                                {Math.round(weatherInfo.weather.main.temp_max)}°
-                                C
-                            </p>
-                        </div>
-                        <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
-                            <div className="text-center text-2xl">
-                                <BsSunset className="inline text-5xl" />
-                            </div>
-                            <p className="text-center mt-2 opacity-80">
-                                {format(
-                                    new Date(
-                                        fromUnixTime(
-                                            weatherInfo.weather.sys.sunset
-                                        )
-                                    ),
-                                    "HH:mm"
-                                )}
-                            </p>
-                        </div>
+                    )}
+                    <div>
+                        {data && (
+                            <>
+                                <div className="flex justify-center items-center pt-6 flex-col border-b-2 border-white mb-6 sm:flex-row sm:border-none">
+                                    <div className="m-8">
+                                        {renderIcon(
+                                            data?.weather[0]?.icon,
+                                            4,
+                                            "text-8xl"
+                                        )}
+                                    </div>
+                                    <div className="px-4 mb-8 sm:mb-0">
+                                        <p className="text-5xl font-bold text-center">
+                                            {Math.round(data.main.temp)}° C
+                                        </p>
+                                        <h2 className="text-base flex justify-center items-center mt-2 sm:justify-start">
+                                            <IoLocationOutline className="mr-2 text-2xl" />
+                                            <span className="opacity-80">
+                                                Zagreb, Hrvatska
+                                            </span>
+                                        </h2>
+                                    </div>
+                                </div>
+                                <div className="flex justify-center pb-4 flex-wrap">
+                                    <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
+                                        <div className="text-center text-2xl">
+                                            <BsSunrise className="inline text-5xl" />
+                                        </div>
+                                        <p className="text-center mt-2 opacity-80">
+                                            {format(
+                                                new Date(
+                                                    fromUnixTime(
+                                                        data.sys.sunrise
+                                                    )
+                                                ),
+                                                "HH:mm"
+                                            )}
+                                        </p>
+                                    </div>
+                                    <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
+                                        <div className="text-center text-2xl">
+                                            <FaTemperatureHigh className="inline text-5xl" />
+                                        </div>
+                                        <p className="text-center mt-2 opacity-80">
+                                            {Math.round(data.main.temp_min)}° C
+                                        </p>
+                                    </div>
+                                    <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
+                                        <div className="text-center text-2xl">
+                                            <BsWind className="inline text-5xl" />
+                                        </div>
+                                        <p className="text-center mt-2 opacity-80">
+                                            {(data.wind.speed * 3.6).toFixed(2)}{" "}
+                                            km/h
+                                        </p>
+                                    </div>
+                                    <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
+                                        <div className="text-center text-2xl">
+                                            <FaTemperatureLow className="inline text-5xl" />
+                                        </div>
+                                        <p className="text-center mt-2 opacity-80">
+                                            {Math.round(data.main.temp_max)}° C
+                                        </p>
+                                    </div>
+                                    <div className="flex align-center flex-col p-4 grow w-1/2 sm:w-auto sm:max-w-fit md:mx-6">
+                                        <div className="text-center text-2xl">
+                                            <BsSunset className="inline text-5xl" />
+                                        </div>
+                                        <p className="text-center mt-2 opacity-80">
+                                            {format(
+                                                new Date(
+                                                    fromUnixTime(
+                                                        data.sys.sunset
+                                                    )
+                                                ),
+                                                "HH:mm"
+                                            )}
+                                        </p>
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </>
             )}
--- src/components/ForecastWeather.tsx
+++ src/components/ForecastWeather.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {
     BsFillSunFill,
@@ -7,69 +6,40 @@ import {
 } from "react-icons/bs";
 import { fromUnixTime, format } from "date-fns";
 
-interface IFiveDayWeather {
-    fiveDayWeather: [
+import { apiKey, useAxios } from "../api/hookAxios";
+
+interface IFiveDayWeatherItem {
+    dt_txt: string;
+    dt: number;
+    main: {
+        temp: number;
+    };
+    weather: [
         {
-            dt_txt: string;
-            dt: number;
-            main: {
-                temp: number;
-            };
-            weather: [
-                {
-                    main: string;
-                    description: string;
-                    icon: string;
-                }
-            ];
+            main: string;
+            description: string;
+            icon: string;
         }
     ];
-    errorMsg: string;
+}
+
+interface IFiveDayWeatherState {
+    list: [];
 }
 
 const ForecastWeather = () => {
-    const url = "https://api.openweathermap.org/";
-    const apiKey = "bfd14f25f12b27477818a553e86af0d4";
+    const { response, loading, error }: any = useAxios({
+        method: "GET",
+        url: `data/2.5/forecast?q=Zagreb&units=metric&lang=hr&appid=${apiKey}`,
+    });
 
-    const [fiveDayWeatherInfo, setFiveDayWeatherInfo] =
-        useState<IFiveDayWeather>({
-            fiveDayWeather: [
-                {
-                    dt_txt: "",
-                    dt: 0,
-                    main: {
-                        temp: 0,
-                    },
-                    weather: [
-                        {
-                            main: "",
-                            description: "",
-                            icon: "",
-                        },
-                    ],
-                },
-            ],
-            errorMsg: "",
-        });
+    const [data, setData] = useState<IFiveDayWeatherState>({ list: [] });
 
     useEffect(() => {
-        axios
-            .get(
-                `${url}data/2.5/forecast?q=Zagreb&units=metric&lang=hr&appid=${apiKey}`
-            )
-            .then((res) => {
-                setFiveDayWeatherInfo({
-                    ...fiveDayWeatherInfo,
-                    fiveDayWeather: res.data.list,
-                });
-            })
-            .catch((err) => {
-                setFiveDayWeatherInfo({
-                    ...fiveDayWeatherInfo,
-                    errorMsg: err.message,
-                });
-            });
-    }, []);
+        if (response !== null) {
+            setData(response);
+        }
+    }, [response]);
 
     const renderIcon = (
         iconCode: string,
@@ -93,61 +63,82 @@ const ForecastWeather = () => {
     };
 
     return (
-        <div className="App">
-            <section className="py-6">
-                {fiveDayWeatherInfo.errorMsg ? (
-                    <div className="m-auto text-white">
-                        <p className="flex justify-center items-center flex-wrap">
-                            <span className="text-4xl flex items-center">
-                                <BsEmojiFrown className="mr-2" /> Oh no!
-                            </span>
-                            <span className="block w-full text-center mt-4">
-                                {fiveDayWeatherInfo.errorMsg}
-                            </span>
-                        </p>
+        <section className="py-6">
+            {loading ? (
+                <p>loading...</p>
+            ) : (
+                <div>
+                    {error && (
+                        <div className="m-auto text-white">
+                            <p className="flex justify-center items-center flex-wrap">
+                                <span className="text-4xl flex items-center">
+                                    <BsEmojiFrown className="mr-2" /> Oh no!
+                                </span>
+                                <span className="block w-full text-center mt-4">
+                                    {error.message}
+                                </span>
+                            </p>
+                        </div>
+                    )}
+                    <div>
+                        {data && (
+                            <ul className="flex overflow-x-scroll">
+                                {data.list.map(
+                                    (item: IFiveDayWeatherItem, i: number) => {
+                                        return (
+                                            <li
+                                                className="max-w-xs px-8 py-4 mx-auto bg-white/[.2] rounded-lg shadow-md my-2 flex-auto backdrop-blur-lg min-w-max text-white mr-2 last-of-type:mr-0"
+                                                key={i}
+                                            >
+                                                <span className="block text-2xl text-center my-4 font-bold">
+                                                    {Math.round(item.main.temp)}
+                                                    ° C
+                                                </span>
+                                                <div className="m-8 mt-4 flex justify-center items-center w-24 h-24">
+                                                    {renderIcon(
+                                                        item.weather[0].icon,
+                                                        2,
+                                                        "text-5xl"
+                                                    )}
+                                                </div>
+                                                <p className="opacity-80">
+                                                    <span className="block text-center">
+                                                        {format(
+                                                            new Date(
+                                                                fromUnixTime(
+                                                                    item.dt
+                                                                )
+                                                            ),
+                                                            "dd.MM.yyyy"
+                                                        )}
+                                                    </span>
+                                                    <span className="block text-center">
+                                                        {format(
+                                                            new Date(
+                                                                fromUnixTime(
+                                                                    item.dt
+                                                                )
+                                                            ),
+                                                            "HH:mm"
+                                                        )}
+                                                    </span>
+                                                </p>
+                                                <span className="block uppercase text-center my-4 bg-white text-gray-800 text-xs font-semibold mr-2 px-2.5 py-1.5 rounded">
+                                                    {
+                                                        item.weather[0]
+                                                            .description
+                                                    }
+                                                </span>
+                                            </li>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        )}
                     </div>
-                ) : (
-                    <ul className="flex overflow-x-scroll">
-                        {fiveDayWeatherInfo.fiveDayWeather.map((item, i) => {
-                            return (
-                                <li
-                                    className="max-w-xs px-8 py-4 mx-auto bg-white/[.2] rounded-lg shadow-md my-2 flex-auto backdrop-blur-lg min-w-max text-white mr-2 last-of-type:mr-0"
-                                    key={i}
-                                >
-                                    <span className="block text-2xl text-center my-4 font-bold">
-                                        {Math.round(item.main.temp)}° C
-                                    </span>
-                                    <div className="m-8 mt-4 flex justify-center items-center w-24 h-24">
-                                        {renderIcon(
-                                            item.weather[0].icon,
-                                            2,
-                                            "text-5xl"
-                                        )}
-                                    </div>
-                                    <p className="opacity-80">
-                                        <span className="block text-center">
-                                            {format(
-                                                new Date(fromUnixTime(item.dt)),
-                                                "dd.MM.yyyy"
-                                            )}
-                                        </span>
-                                        <span className="block text-center">
-                                            {format(
-                                                new Date(fromUnixTime(item.dt)),
-                                                "HH:mm"
-                                            )}
-                                        </span>
-                                    </p>
-                                    <span className="block uppercase text-center my-4 bg-white text-gray-800 text-xs font-semibold mr-2 px-2.5 py-1.5 rounded">
-                                        {item.weather[0].description}
-                                    </span>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                )}
-            </section>
-        </div>
+                </div>
+            )}
+        </section>
     );
 };
 
