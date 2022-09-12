0d4b49c9dbb7c7850ecf87839cdbf935969240af
marko <krznarm26@gmail.com>
2022-09-12 21:45:15 +0200

    Fixed spacing & added ts events

--- src/App.tsx
+++ src/App.tsx
@@ -7,72 +7,71 @@ import { InputCity } from "./components/InputCity";
 import { apiKey } from "./api/hookAxios";
 
 const App = () => {
-    const [state, setState] = useState("");
+	const [state, setState] = useState("");
 
-    const [cityInfo, setCityInfo] = useState<any>({});
+	const [cityInfo, setCityInfo] = useState<any>({});
 
-    const isEmpty = Object.keys(cityInfo).length === 0;
+	const isEmpty = Object.keys(cityInfo).length === 0;
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
+	const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+		event.preventDefault();
 
-        const axios = require("axios");
+		const axios = require("axios");
 
-        const fetchCity = () => {
-            axios
-                .get(
-                    `https://api.openweathermap.org/geo/1.0/direct?q=${state}&limit=1&units=metric&appid=${apiKey}`
-                )
+		const fetchCity = () => {
+			axios
+				.get(
+					`https://api.openweathermap.org/geo/1.0/direct?q=${state}&limit=1&units=metric&appid=${apiKey}`
+				)
 
-                .then((response: any) => {
-                    if (response.data !== null && response.data !== undefined) {
-                        setCityInfo(response.data[0]);
-                    }
-                })
+				.then((response: any) => {
+					if (response.data !== null && response.data !== undefined) {
+						setCityInfo(response.data[0]);
+					}
+				})
 
-                .catch((err: any) => {
-                    console.log(err);
-                });
-        };
+				.catch((err: any) => {
+					console.log(err);
+				});
+		};
 
-        fetchCity();
+		fetchCity();
 
-        setState("");
-        setCityInfo({});
-    };
+		setState("");
 
-    const handleChange = (event: any) => {
-        const trimedTerm = event.target.value.trim();
+		setCityInfo({});
+	};
 
-        setState(trimedTerm);
-    };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setState(event.target.value);
+	};
 
-    const handleClear = () => {
-        setState("");
-    };
+	const handleClear = () => {
+		setState("");
+	};
 
-    return (
-        <div className="App px-4">
-            <h2 className="text-white text-5xl text-center font-bold my-8">
-                Weather by city
-            </h2>
-            <InputCity
-                handleSubmit={handleSubmit}
-                handleChange={handleChange}
-                handleClear={handleClear}
-                state={state}
-                cityInfo={cityInfo}
-            />
-            {isEmpty === false ? (
-                <>
-                    <CurrentWeather cityInfo={cityInfo} />
-                    <ForecastWeather cityInfo={cityInfo} />
-                </>
-            ) : (
-                <></>
-            )}
-        </div>
-    );
+	return (
+		<div className="App px-4">
+			<h2 className="text-white text-5xl text-center font-bold my-8">
+				Weather by city
+			</h2>
+			<InputCity
+				handleSubmit={handleSubmit}
+				handleChange={handleChange}
+				handleClear={handleClear}
+				state={state}
+				cityInfo={cityInfo}
+			/>
+			{isEmpty === false ? (
+				<>
+					<CurrentWeather cityInfo={cityInfo} />
+					<ForecastWeather cityInfo={cityInfo} />
+				</>
+			) : (
+				<></>
+			)}
+		</div>
+	);
 };
 
 export default App;
