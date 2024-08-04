432bba4518709b27c3d06c66485693b0fafc98bc
marko <krznarm26@gmail.com>
2024-08-04 19:16:11 +0200

    Added WeatherHighlights component

---
+++ src/components/WeatherHighlights.tsx
@@ -0,0 +1,72 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faWind,
+	faTemperatureArrowUp,
+	faTemperatureArrowDown,
+	faDroplet,
+	faEye,
+	faCloud,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { currentWeather } from "../App";
+import CardItem from "../components/CardItem";
+
+function WeatherHighlights() {
+	const weatherArray = [
+		{
+			iconName: <FontAwesomeIcon icon={faWind} />,
+			itemName: "Wind",
+			value: currentWeather.wind.speed,
+			measureUnit: "km/h",
+		},
+		{
+			iconName: <FontAwesomeIcon icon={faTemperatureArrowUp} />,
+			itemName: "Max Temperature",
+			value: currentWeather.main.temp_max,
+			measureUnit: "°C",
+		},
+		{
+			iconName: <FontAwesomeIcon icon={faTemperatureArrowDown} />,
+			itemName: "Min Temperature",
+			value: currentWeather.main.temp_min,
+			measureUnit: "°C",
+		},
+		{
+			iconName: <FontAwesomeIcon icon={faDroplet} />,
+			itemName: "Humidity",
+			value: currentWeather.main.humidity,
+			measureUnit: "%",
+		},
+		{
+			iconName: <FontAwesomeIcon icon={faEye} />,
+			itemName: "Visibility",
+			value: currentWeather.visibility,
+			measureUnit: "km",
+		},
+		{
+			iconName: <FontAwesomeIcon icon={faCloud} />,
+			itemName: "Pressure",
+			value: currentWeather.main.pressure,
+			measureUnit: "hpa",
+		},
+	];
+
+	return (
+		<div className="wa-cards">
+			<span className="wa-title">Today's Highlights</span>
+			<div className="wa-cards-wrapper">
+				{weatherArray.map((weatherItem) => (
+					<CardItem
+						key={weatherItem.itemName}
+						icon={weatherItem.iconName}
+						item={weatherItem.itemName}
+						value={weatherItem.value}
+						mesure={weatherItem.measureUnit}
+					/>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default WeatherHighlights;
