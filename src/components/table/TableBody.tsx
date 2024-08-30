2fdef2fcd18a9ba5aae9c7e2d6547a4cedba455e
marko <krznarm26@gmail.com>
2024-08-30 21:52:32 +0200

    Refactored TableRow component - added interfaces - added functions from TableBody component - simplified logic

--- src/components/table/TableBody.tsx
+++ src/components/table/TableBody.tsx
@@ -2,51 +2,17 @@
 import TableRow from "./TableRow";
 
 function TableBody({ groupedDataByDate }: any) {
-	const formatDate = (dateString: string) => {
-		const date = new Date(dateString);
-		const today = new Date();
-
-		// Check if the given date is today
-		const isToday = date.toDateString() === today.toDateString();
-
-		// Define options for formatting the day and month
-		const options: any = { day: "numeric", month: "short" };
-
-		// Get the formatted day and month (e.g., "30 Aug.")
-		const dayMonth = date.toLocaleDateString("en-GB", options);
-
-		if (isToday) {
-			return `Today ${dayMonth}.`;
-		} else {
-			// Get the weekday (e.g., "Friday")
-			const weekday = date.toLocaleDateString("en-GB", {
-				weekday: "long",
-			});
-			return `${weekday} ${dayMonth}.`;
-		}
-	};
-
 	return (
 		<div className="wa-table-body-wrapper">
 			{groupedDataByDate.map((item: any, index: number) => {
 				if (item.data.length < 8 && index === 0) {
 					const placeholderNumber: number = 8 - item.data.length;
 
-					const renderPlaceholders = Array.from(
-						{
-							length: placeholderNumber,
-						},
-						() => (
-							<div className="wa-column wa-column-placeholder" />
-						)
-					);
-
 					return (
 						<TableRow
 							key={index}
 							item={item}
-							formatDate={formatDate}
-							renderPlaceholders={renderPlaceholders}
+							placeholderNumber={placeholderNumber}
 						/>
 					);
 				}
@@ -54,28 +20,16 @@ function TableBody({ groupedDataByDate }: any) {
 				if (item.data.length < 8 && index === 5) {
 					const placeholderNumber: number = 8 - item.data.length;
 
-					const renderPlaceholders = Array.from(
-						{
-							length: placeholderNumber,
-						},
-						() => (
-							<div className="wa-column wa-column-placeholder" />
-						)
-					);
-
 					return (
 						<TableRow
 							key={index}
 							item={item}
-							formatDate={formatDate}
-							renderPlaceholders={renderPlaceholders}
+							placeholderNumber={placeholderNumber}
 						/>
 					);
 				}
 
-				return (
-					<TableRow key={index} item={item} formatDate={formatDate} />
-				);
+				return <TableRow key={index} item={item} />;
 			})}
 		</div>
 	);
--- src/components/table/TableRow.tsx
+++ src/components/table/TableRow.tsx
@@ -1,27 +1,73 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* FUTURE add columns
 <div className="wa-column wa-column-temp">27°C / 32°C</div>
 <div className="wa-column">3.9mm</div>
 <div className="wa-column">1m/s</div>
 */
-function TableRow({ item, index, formatDate, renderPlaceholders }: any) {
+
+interface TableRowProps {
+	item: {
+		date: string;
+		data: Array<HourWeatherProps>;
+	};
+	key: number;
+	placeholderNumber?: number;
+}
+
+interface HourWeatherProps {
+	weather: Array<{
+		icon: string;
+		description: string;
+	}>;
+	dt: number;
+}
+
+function TableRow({ item, placeholderNumber }: TableRowProps) {
+	const formatDate = (dateString: string) => {
+		const date = new Date(dateString);
+		const today = new Date();
+
+		const isToday = date.toDateString() === today.toDateString();
+
+		const dayMonth = date.toLocaleDateString("en-GB", {
+			day: "numeric",
+			month: "short",
+		});
+
+		if (isToday) {
+			return `Today ${dayMonth}.`;
+		} else {
+			const weekday = date.toLocaleDateString("en-GB", {
+				weekday: "long",
+			});
+
+			return `${weekday} ${dayMonth}.`;
+		}
+	};
+
+	const renderPlaceholders =
+		placeholderNumber &&
+		Array.from({ length: placeholderNumber }, () => (
+			<div className="wa-column wa-column-placeholder" />
+		));
+
 	return (
-		<div key={item.date + index} className="wa-table-row-wrapper">
+		<div key={item.date} className="wa-table-row-wrapper">
 			<div className="wa-column wa-column-today">
 				{formatDate(item.date)}
 			</div>
 			<div className="wa-table-columns-wrapper">
 				{renderPlaceholders}
-				{item.data.map((hourWeather: any) => {
-					return (
-						<div className="wa-column wa-img-wrapper">
-							<img
-								src={`https://openweathermap.org/img/wn/${hourWeather.weather[0].icon}@4x.png`}
-								alt={hourWeather.weather[0].description}
-							/>
-						</div>
-					);
-				})}
+				{item.data.map((hourWeather: HourWeatherProps) => (
+					<div
+						key={hourWeather.dt}
+						className="wa-column wa-img-wrapper"
+					>
+						<img
+							src={`https://openweathermap.org/img/wn/${hourWeather.weather[0].icon}@4x.png`}
+							alt={hourWeather.weather[0].description}
+						/>
+					</div>
+				))}
 			</div>
 		</div>
 	);
