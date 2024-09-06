da9c18210e8d60ecdde5702bb811f3f2ee8f1360
marko <krznarm26@gmail.com>
2024-09-06 23:28:29 +0200

    Mobile app redesign

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -1,10 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+	faLocationDot,
+	faCircleUp,
+	faCircleDown,
+	faTemperatureThreeQuarters,
+} from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { RootStore, AppDispatch } from "../state/store";
 import { useEffect } from "react";
 import { fetchWeatherByCity } from "../state/weather/weatherSlice";
 import { fetchFiveDayForecastByCity } from "../state/weather/fiveDayForecastSlice";
+import { formatTimestampToHours } from "../utils/dateUtils";
 
 function WeatherInfo() {
 	const weatherState = useSelector((state: RootStore) => state.weather);
@@ -34,17 +40,33 @@ function WeatherInfo() {
 				<FontAwesomeIcon icon={faLocationDot} />
 				<span>{`${weatherState?.data?.name}, ${weatherState?.data?.sys.country}`}</span>
 			</div>
-			<div>
+			<div className="wa-image-wrapper">
 				<img
 					src={`https://openweathermap.org/img/wn/${weatherState?.data?.weather[0].icon}@4x.png`}
 					alt={weatherState?.data?.weather[0].description}
 				/>
 			</div>
-			<p className="wa-value">
-				{weatherState?.data?.main.temp.toFixed(0)}
-				<span className="wa-mesure-sign">°C</span>
-			</p>
-			<p>{weatherState?.data?.weather[0].main}</p>
+			<div className="wa-curreny-content-wrapper">
+				<p className="wa-value">
+					{weatherState?.data?.main.temp.toFixed(0)}
+					<span className="wa-mesure-sign">°C</span>
+				</p>
+				<p>{weatherState?.data?.weather[0].main}</p>
+			</div>
+			<div className="wa-current-weather-info">
+				<p className="wa-text">
+					<FontAwesomeIcon icon={faTemperatureThreeQuarters} />
+					{weatherData.main.feels_like.toFixed()} °C
+				</p>
+				<p className="wa-text">
+					<FontAwesomeIcon icon={faCircleUp} />
+					{formatTimestampToHours(weatherData.sys.sunrise) + " h"}
+				</p>
+				<p className="wa-text">
+					<FontAwesomeIcon icon={faCircleDown} />
+					{formatTimestampToHours(weatherData.sys.sunset) + " h"}
+				</p>
+			</div>
 		</div>
 	);
 }
