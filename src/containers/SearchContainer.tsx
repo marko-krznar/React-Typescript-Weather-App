01573d841ebe6b62e72adba09881fb3ee325b393
marko <krznarm26@gmail.com>
2024-10-10 21:30:23 +0200

    Refactor search element to form element

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -6,6 +6,23 @@ import WeatherTimeline from "../components/WeatherTimeline";
 function Search() {
 	return (
 		<div className="wa-container wa-search-container">
+			<form
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					width: "100%",
+				}}
+			>
+				<input
+					style={{
+						flex: "1",
+						padding: "12px 40px",
+					}}
+					type="text"
+					placeholder="Search for place..."
+				/>
+				<button type="submit">Find</button>
+			</form>
 			<SearchForm />
 			<Divider />
 			<WeatherInfo />
