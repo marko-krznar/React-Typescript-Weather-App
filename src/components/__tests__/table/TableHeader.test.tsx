d706e3f74410df013592ac371c24b4a966ce125d
Marko Krznar <71086905+marko-krznar@users.noreply.github.com>
2024-10-24 20:41:28 +0200

    Added testing configuration and tests for components (#11)
    - added packages for testing in react
    - setup testing configuration
    - add test for Divider component
    
    Added tests and fixed jest.config.js globals issue
    - FiveDayForecast
    - IntroSection
    - SearchForm
    - WeatherHighlightCard
    - WeatherHighlights
    - WeatherInfo
    - WeatherTimeline
    
    Added tests for table
    - TableBody
    - TableHeader
    - TableRow
    
    * Fix test renders the placeholder div
    
    * Fix 'should clear search input when the clear button is clicked' test
    
    * Fix 'should dispatch fetchFiveDayForecastByCity on mount' test
    
    * Fix 'WeatherHighlightCard' undefined value issue
    
    * Fix 'dispatches fetchWeatherByCity and fetchFiveDayForecastByCity on mount' test
    
    * Fix 'displays the date correctly based on the Unix timestamp' test
    
    * Fix tests in 'TableBody.test.tsx'
    
    * Fix tests in 'TableRow.test'
    
    * Fix tests in 'TableRow.test.tsx'

---
+++ src/components/__tests__/table/TableHeader.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import TableHeader from "../../table/TableHeader";
+
+describe("TableHeader", () => {
+	it("renders the time columns correctly", () => {
+		render(<TableHeader />);
+		const timeColumns = [
+			"00:00",
+			"03:00",
+			"06:00",
+			"09:00",
+			"12:00",
+			"15:00",
+			"18:00",
+			"21:00",
+		];
+		timeColumns.forEach((time) => {
+			const column = screen.getByText(time);
+			expect(column).toBeInTheDocument();
+		});
+	});
+
+	it("renders the placeholder div", () => {
+		render(<TableHeader />);
+		const placeholder = document.querySelector(".wa-placeholder");
+		expect(placeholder).toBeInTheDocument();
+	});
+});
